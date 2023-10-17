f122a09747da22cd12d9cf8abfa2ced2227e05b5
Ava Pek <code@exodrifter.space>
2023-10-17 11:35:01 -0700

    Add Identifier doctests
    This required the use of doctest-cabal so that doctest could find the
    dependencies.

--- doctests/Main.hs
+++ doctests/Main.hs
@@ -1,11 +1,7 @@
 module Main where
 
+import Build_doctests (flags, pkgs, module_sources)
 import Test.DocTest (doctest)
 
 main :: IO ()
-main =
-  doctest
-    [ "-XBlockArguments"
-    , "--fast"
-    , "src"
-    ]
+main = doctest (flags <> pkgs <> module_sources)
--- src/Rumor/Parser/Identifier.hs
+++ src/Rumor/Parser/Identifier.hs
@@ -5,11 +5,50 @@ module Rumor.Parser.Identifier
 import Data.Char (isLetter, isMark, isDigit)
 import Data.Text (Text)
 import Rumor.Parser.Common (Parser)
+import Text.Megaparsec ((<?>))
 
 import qualified Text.Megaparsec as Mega
 
+-- $setup
+-- >>> import qualified Text.Megaparsec as Mega
+-- >>> let parseTest inner = Mega.parseTest (inner <* Mega.hidden Mega.eof)
+
+{-| Parses an identifier, which is defined as any consecutive sequence of
+  letters, digits, marks, underscores, and dashes.
+
+  Examples:
+  >>> parseTest identifier "alice"
+  "alice"
+
+  >>> parseTest identifier "アリス"
+  "\12450\12522\12473"
+
+  >>> parseTest identifier "alice-アリス"
+  "alice-\12450\12522\12473"
+
+  >>> parseTest identifier "alice_アリス"
+  "alice_\12450\12522\12473"
+
+  >>> parseTest identifier "alice アリス"
+  1:6:
+    |
+  1 | alice アリス
+    |      ^
+  unexpected space
+  expecting valid identifier character
+-}
 identifier :: Parser Text
 identifier =
-  Mega.takeWhile1P
-    (Just "letter, mark, or digit character")
-    (\ch -> isLetter ch || isMark ch || isDigit ch)
+  let
+    validChar ch =
+         isLetter ch
+      || isMark ch
+      || isDigit ch
+      || ch == '-'
+      || ch == '_'
+    ident =
+      Mega.takeWhile1P
+        (Just "valid identifier character")
+        validChar
+  in
+    ident <?> "identifier"
