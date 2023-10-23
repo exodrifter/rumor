c8551006a23e21415bd583db82f9a0fd7d0f9646
Ava Pek <code@exodrifter.space>
2023-10-23 08:38:50 -0700

    Add let doctests

--- src/Rumor/Parser/Let.hs
+++ src/Rumor/Parser/Let.hs
@@ -11,10 +11,109 @@ import qualified Text.Megaparsec.Char as Char
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 
 -- $setup
+-- >>> import Data.Either (fromRight)
+-- >>> import Data.NonEmptyText as NET
 -- >>> import Rumor.Parser.Common
--- >>> let parse inner = parseTest newContext (inner <* eof)
+-- >>> import Rumor.Internal.Types
+--
+-- >>> :{
+-- let setVariableTypes c0 = do
+--       c1 <- setVariableType (VariableName (Unicode (NET.new 's' "tring"))) StringType c0
+--       c2 <- setVariableType (VariableName (Unicode (NET.new 'n' "umber"))) NumberType c1
+--       c3 <- setVariableType (VariableName (Unicode (NET.new 'b' "oolean"))) BooleanType c2
+--       pure c3
+-- :}
+--
+-- >>> let context = fromRight undefined (setVariableTypes newContext)
+-- >>> let parse inner = parseTest context (inner <* eof)
 
-let' :: Parser ()
+{-| Parses a let binding, which can be used to assign types to variables.
+
+  >>> parse let' "let foo: Boolean"
+  (VariableName (Unicode "foo"),BooleanType)
+
+  >>> parse let' "let foo: Number"
+  (VariableName (Unicode "foo"),NumberType)
+
+  >>> parse let' "let foo: String"
+  (VariableName (Unicode "foo"),StringType)
+
+  You cannot change the types of already-defined variables.
+
+  >>> parse let' "let boolean: Number"
+  1:1:
+    |
+  1 | let boolean: Number
+    | ^^^^^^^^^^^^^^^^^^^
+  Variable `boolean` cannot be a Number; it has already been defined as a Boolean!
+
+  >>> parse let' "let boolean: String"
+  1:1:
+    |
+  1 | let boolean: String
+    | ^^^^^^^^^^^^^^^^^^^
+  Variable `boolean` cannot be a String; it has already been defined as a Boolean!
+
+  >>> parse let' "let number: Boolean"
+  1:1:
+    |
+  1 | let number: Boolean
+    | ^^^^^^^^^^^^^^^^^^^
+  Variable `number` cannot be a Boolean; it has already been defined as a Number!
+
+  >>> parse let' "let number: String"
+  1:1:
+    |
+  1 | let number: String
+    | ^^^^^^^^^^^^^^^^^^
+  Variable `number` cannot be a String; it has already been defined as a Number!
+
+  >>> parse let' "let string: Boolean"
+  1:1:
+    |
+  1 | let string: Boolean
+    | ^^^^^^^^^^^^^^^^^^^
+  Variable `string` cannot be a Boolean; it has already been defined as a String!
+
+  >>> parse let' "let string: Number"
+  1:1:
+    |
+  1 | let string: Number
+    | ^^^^^^^^^^^^^^^^^^
+  Variable `string` cannot be a Number; it has already been defined as a String!
+
+  You can re-define variables to the same type.
+
+  >>> parse let' "let boolean: Boolean"
+  (VariableName (Unicode "boolean"),BooleanType)
+
+  >>> parse let' "let number: Number"
+  (VariableName (Unicode "number"),NumberType)
+
+  >>> parse let' "let string: String"
+  (VariableName (Unicode "string"),StringType)
+
+  You can have extra horizontal whitespace around the colon, but not vertical
+  whitespace.
+
+  >>> parse let' "let foobar:String"
+  (VariableName (Unicode "foobar"),StringType)
+
+  >>> parse let' "let foobar  :  String"
+  (VariableName (Unicode "foobar"),StringType)
+
+  >>> parse let' "let foobar\t:\tString"
+  (VariableName (Unicode "foobar"),StringType)
+
+  >>> parse let' "let foobar\n:\nString"
+  1:11:
+    |
+  1 | let foobar
+    |           ^
+  unexpected newline
+  expecting ':'
+-}
+let' :: Parser (Rumor.VariableName, Rumor.Type)
 let' = do
   let parser = do
         start <- Mega.getOffset
@@ -28,12 +127,16 @@ let' = do
           end <- Mega.getOffset
 
           result <- modifyVariableType name typ
-          pure (result, end - start)
+          case result of
+            Left err ->
+              pure (Left err, end - start)
+            Right () ->
+              pure (Right (name, typ), end - start)
 
   result <- Mega.lookAhead parser <?> "variable"
   case result of
     (Left err, len) ->
       rumorError err len
-    (Right (), len) -> do
+    (Right r, len) -> do
       _ <- Mega.takeP Nothing len
-      pure ()
+      pure r
