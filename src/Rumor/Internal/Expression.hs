db08548f152ebd7be6d390fa3dd6b455ba7fc391
Ava Pek <code@exodrifter.space>
2023-10-26 12:04:02 -0700

    Slightly better simplification logic

--- src/Rumor/Internal/Expression.hs
+++ src/Rumor/Internal/Expression.hs
@@ -15,6 +15,12 @@ import qualified Data.Maybe as Maybe
 import qualified Data.Scientific as S
 import qualified Data.Text as T
 
+-- $setup
+-- >>> import qualified Data.NonEmptyText as NET
+-- >>> import Rumor.Internal.VariableName
+-- >>> import Rumor.Internal.Unicode
+-- >>> let foo = VariableName (Unicode (NET.new 'f' "oo"))
+
 -- | Represents expressions in a Rumor dialog.
 data Expression typ where
   String :: Text -> Expression Text
@@ -61,52 +67,117 @@ instance Monoid (Expression Text) where
       [x] -> x
       x:rest -> x <> mconcat rest
 
+{-| Simplifies expressions until they can no longer be reduced.
+
+  -- TODO: This simplification logic could be better.
+
+  >>> simplify (LogicalAnd (Boolean True) (Boolean True))
+  Boolean True
+
+  >>> simplify (LogicalAnd (LogicalAnd (Boolean True) (Boolean True)) (LogicalAnd (Boolean True) (Boolean True)))
+  Boolean True
+
+  >>> simplify (Addition (Number 1) (Number 2))
+  Number 3.0
+
+  >>> simplify (Addition (Addition (Number 1) (Number 2)) (Addition (Number 3) (Number 4)))
+  Number 10.0
+
+  >>> simplify (Concat (String "1") (String "2"))
+  String "12"
+
+  >>> simplify (Concat (Concat (String "1") (String "2")) (Concat (String "3") (String "4")))
+  String "1234"
+
+  It cannot simplify expressions containing variables.
+
+  >>> simplify (LogicalAnd (BooleanVariable foo) (Boolean True))
+  LogicalAnd (BooleanVariable (VariableName (Unicode "foo"))) (Boolean True)
+
+  >>> simplify (LogicalAnd (LogicalAnd (BooleanVariable foo) (Boolean True)) (LogicalAnd (Boolean True) (Boolean True)))
+  LogicalAnd (LogicalAnd (BooleanVariable (VariableName (Unicode "foo"))) (Boolean True)) (Boolean True)
+
+  >>> simplify (Addition (NumberVariable foo) (Number 2))
+  Addition (NumberVariable (VariableName (Unicode "foo"))) (Number 2.0)
+
+  >>> simplify (Addition (Addition (NumberVariable foo) (Number 2)) (Addition (Number 3) (Number 4)))
+  Addition (Addition (NumberVariable (VariableName (Unicode "foo"))) (Number 2.0)) (Number 7.0)
+
+  >>> simplify (Concat (StringVariable foo) (String "2"))
+  Concat (StringVariable (VariableName (Unicode "foo"))) (String "2")
+
+  >>> simplify (Concat (Concat (StringVariable foo) (String "2")) (Concat (String "3") (String "4")))
+  Concat (Concat (StringVariable (VariableName (Unicode "foo"))) (String "2")) (String "34")
+
+-}
 simplify :: Expression typ -> Expression typ
 simplify expression =
   case expression of
-    Concat (String l) (String r) -> String (l <> r)
-
-    Addition (Number l) (Number r) -> Number (l + r)
-    Subtraction (Number l) (Number r) -> Number (l - r)
-    Division (Number l) (Number r) -> Number (l * r)
-    Multiplication (Number l) (Number r) -> Number (l * r)
-    NumberToString (Number n) -> String (numberToString n)
-
-    LogicalNot (Boolean b) -> Boolean (not b)
-    LogicalAnd (Boolean l) (Boolean r) -> Boolean (l && r)
-    LogicalOr (Boolean l) (Boolean r) -> Boolean (l || r)
-    LogicalXor (Boolean l) (Boolean r) -> Boolean (l /= r)
-    BooleanToString (Boolean b) -> String (booleanToString b)
-
-    Equal (String l) (String r) -> Boolean (l == r)
-    NotEqual (String l) (String r) -> Boolean (l /= r)
-    Equal (Number l) (Number r) -> Boolean (l == r)
-    NotEqual (Number l) (Number r) -> Boolean (l /= r)
-    Equal (Boolean l) (Boolean r) -> Boolean (l == r)
-    NotEqual (Boolean l) (Boolean r) -> Boolean (l /= r)
-
     String _ -> expression
     StringVariable _ -> expression
-    Concat _ _ -> expression
+    Concat lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (String l, String r) -> String (l <> r)
+        (l, r) -> Concat l r
 
     Number _ -> expression
     NumberVariable _ -> expression
-    Addition _ _ -> expression
-    Subtraction _ _ -> expression
-    Division _ _ -> expression
-    Multiplication _ _ -> expression
-    NumberToString _ -> expression
+    Addition lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Number l, Number r) -> Number (l + r)
+        (l, r) -> Addition l r
+    Subtraction lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Number l, Number r) -> Number (l + r)
+        (l, r) -> Subtraction l r
+    Multiplication lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Number l, Number r) -> Number (l + r)
+        (l, r) -> Multiplication l r
+    Division lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Number l, Number r) -> Number (l + r)
+        (l, r) -> Division l r
+    NumberToString inner ->
+      case simplify inner of
+        Number n -> String (numberToString n)
+        n -> NumberToString n
 
     Boolean _ -> expression
     BooleanVariable _ -> expression
-    LogicalNot _ -> expression
-    LogicalAnd _ _ -> expression
-    LogicalOr _ _ -> expression
-    LogicalXor _ _ -> expression
-    BooleanToString _ -> expression
+    LogicalNot inner ->
+      case simplify inner of
+        Boolean b -> Boolean (not b)
+        b -> LogicalNot b
+    LogicalAnd lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Boolean l, Boolean r) -> Boolean (l && r)
+        (l, r) -> LogicalAnd l r
+    LogicalOr lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Boolean l, Boolean r) -> Boolean (l || r)
+        (l, r) -> LogicalOr l r
+    LogicalXor lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (Boolean l, Boolean r) -> Boolean (l /= r)
+        (l, r) -> LogicalXor l r
+    BooleanToString inner ->
+      case simplify inner of
+        Boolean b -> String (booleanToString b)
+        b -> BooleanToString b
 
-    Equal _ _ -> expression
-    NotEqual _ _ -> expression
+    Equal lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (String l, String r) -> Boolean (l == r)
+        (Number l, Number r) -> Boolean (l == r)
+        (Boolean l, Boolean r) -> Boolean (l == r)
+        (l, r) -> Equal l r
+    NotEqual lhs rhs ->
+      case (simplify lhs, simplify rhs) of
+        (String l, String r) -> Boolean (l /= r)
+        (Number l, Number r) -> Boolean (l /= r)
+        (Boolean l, Boolean r) -> Boolean (l /= r)
+        (l, r) -> NotEqual l r
 
 numberToString :: Scientific -> Text
 numberToString number =
